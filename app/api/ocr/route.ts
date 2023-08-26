6caba20154342380a4c0e873bd93cecfafdf66f9
Bryan Tay <bryan.tay@autodesk.com>
2023-08-26 20:18:09 +0800

    for andrew

--- app/api/ocr/route.ts
+++ app/api/ocr/route.ts
@@ -6,7 +6,6 @@ export async function POST(request: Request) {
     // Using the OCR.space default free API key (max 10reqs in 10mins) + remote file
     const { image } = await request.json()
     const res1 = await ocrSpace(image, { apiKey: process.env.OCR_KEY })
-
     return NextResponse.json({ data: res1.ParsedResults[0].ParsedText })
   } catch (error) {
     console.error(error)
