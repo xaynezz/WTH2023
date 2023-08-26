e9270ca67d70bcdbd78a6010d295c1f6b95e2214
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-08-26 16:14:23 +0800

    fix camera direction

--- app/imgr/page.tsx
+++ app/imgr/page.tsx
@@ -7,7 +7,7 @@ import axios, { AxiosError } from 'axios'
 const videoConstraints = {
   width: 1280,
   height: 720,
-  facingMode: 'user',
+  facingMode: { exact: 'environment' },
 }
 
 export default function OCR() {
--- app/ocr/page.tsx
+++ app/ocr/page.tsx
@@ -8,7 +8,7 @@ import axios, { AxiosError } from 'axios'
 const videoConstraints = {
   width: 1280,
   height: 720,
-  facingMode: 'user',
+  facingMode: { exact: 'environment' },
 }
 
 export default function OCR() {
