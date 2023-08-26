50c6ead97c23b2101d2240dc18427b86969192c8
Bryan Tay <rivenbryan@gmail.com>
2023-08-26 23:32:11 +0800

    added chatgpt

--- app/api/liveaudiohandler_v2/route.ts
+++ app/api/liveaudiohandler_v2/route.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { NextResponse } from 'next/server'
 import { HfInference } from '@huggingface/inference'
+import OpenAI from "openai";
 
 // Convert the Base64 image to a Blob
 function base64ToBlob(base64Data: any, contentType: any) {
@@ -25,34 +26,52 @@ function base64ToBlob(base64Data: any, contentType: any) {
 export async function POST(request: Request) {
   const { images, flag } = await request.json()
 
-  const arrayOfsubString = [
-    'What is written here',
-    'describe to me the surrounding',
-    'What is in-front of me',
-  ]
-
-  const prompt = arrayOfsubString[flag]
-  var finalString = ''
-  for (let i = 0; i <= images.length; i++) {
+  var stringAboutEnvironment = ''
+  for (let i = 0; i < images.length; i++) { // Fixed loop condition
     const image = images[i]
     try {
       const strippedHeader = image.split(',')[1]
-
       var blob = base64ToBlob(strippedHeader, 'image/jpeg')
 
       const inference = new HfInference(process.env.HF_TOKEN)
-
       const response = await inference.imageToText({
         data: blob,
-        // model: 'Salesforce/blip-image-captioning-base',
         model: 'nlpconnect/vit-gpt2-image-captioning',
       })
-
-      console.log(response)
-      finalString += response.generated_text + '\n'
+      stringAboutEnvironment += response.generated_text
     } catch (error) {
-      console.log(error)
+      console.error("Error processing image:", i, error) // More detailed error logging
     }
   }
-  return NextResponse.json({ data: finalString })
+
+  switch (flag) {
+    case 0:
+      try{
+        
+      
+      }
+    case 1:
+      try {
+ 
+        const openai = new OpenAI({
+          apiKey: process.env.GPT_KEY,
+        });
+      
+        const aggregatedString = "With this environment, " + stringAboutEnvironment + " can you describe to a visually impaired person about the surrounding and summarize it with a maximum of 40 words? "
+        const chatCompletion = await openai.chat.completions.create({
+          messages: [{ role: "user", content: aggregatedString}],
+          model: "gpt-3.5-turbo",
+      });
+      
+        return NextResponse.json({ data: chatCompletion.choices[0].message })
+      } catch (error) {
+        console.error("Error calling OpenAI API:", error)
+        return NextResponse.json({ error: "Error processing chat request." })
+      }
+
+    case 2:
+    default:
+      return NextResponse.json({ error: "Invalid flag value." })
+  }
 }
+
